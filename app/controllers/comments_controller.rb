3d813baf4dc830f313bc68ca1002fba8c423220a
Megan Byrne <megan.byrne27@gmail.com>
2014-03-09 14:12:30 -0700

    Add destroy topics, posts, and comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,15 +4,31 @@ class CommentsController < ApplicationController
     @post = @topic.posts.find(params[:post_id])
     @comment = current_user.comments.build(params[:comment])
 
-    @comments = @post.comments
-    @comment.post = @post
+    @comments = @post.comments #sets up comments for render on failed validation
+    @comment.post = @post #assigns the post to the comment being created
     
     authorize! :create, @comment, message: "You need to be signed in to do that."
     if @comment.save
       redirect_to [@topic, @post], notice: "Comment was saved successfully."
     else
       flash[:error] = "There was an error saving your comment. Please try again."
-      render 'posts/show'
+      render 'posts/show' #On failed validation or error, render this view
+    end
+  end
+
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+
+    @comment = @post.comments.find(params[:id])
+
+    authorize! :destroy, @comment, message: "You need to own the comment to delete it."
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error deleting the comment. Try again."
+      redirect_to [@topic, @post]
     end
   end
 end
