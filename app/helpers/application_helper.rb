412def24922bb2822d99fdd159e26d58780d2b34
Megan Byrne <megan.byrne27@gmail.com>
2014-03-14 08:36:38 -0700

    Add User Profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,6 +14,12 @@ module ApplicationHelper
     (redcarpet.render text).html_safe
   end
 
+  def comment_url_helper(comment)
+    post = comment.post
+    topic = post.topic
+    [topic, post, comment]
+  end
+
   def will_paginate(collection_or_options = nil, options = {})
     if collection_or_options.is_a? Hash
       options, collection_or_options = collection_or_options, nil
